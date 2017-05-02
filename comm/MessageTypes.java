7db7b89f8f5ebca5efdf9596eb021e0d6a632010
FeverishPegasus <erikdixon001@gmail.com>
2017-05-01 20:10:01 -0700

    Added new variable called EMPTY_LOCK

--- comm/MessageTypes.java
+++ comm/MessageTypes.java
@@ -8,5 +8,6 @@ public interface MessageTypes {
     public static int CLOSE_TRANS = 4;
 	public static int READ_LOCK = 5;
 	public static int WRITE_LOCK = 6;
+	public static int EMPTY_LOCK = 0;
 	public static int DISPLAY = 7;
 }
